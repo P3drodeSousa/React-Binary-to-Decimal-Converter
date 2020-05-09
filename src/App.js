ca298c47bd77c8085361c1aea55ca507bf1c5bed
tempname1533 <pedro_de_sousa@outlook.com>
2020-05-09 12:00:58 +0200

    Binary to Decimal Converter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [binary, setBinary] = useState("");
+  const [decimalText, setDecimalText] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (binary.match(/^[0-1]+$/g) === null) {
+      setErrorMessage("Enter either 0 or 1");
+      return;
+    }
+    setErrorMessage("");
+    const reversedBinaryText = binary
+      .split("")
+      .map(Number) // Convert to a number from string
+      .reverse();
+
+    console.log(reversedBinaryText);
+    // Calculate the result by accumulating previous vaue
+    const result = reversedBinaryText.reduce(
+      (accumulator, currentValue, idx) =>
+        accumulator + currentValue * Math.pow(2, idx)
+    );
+
+    setDecimalText(result);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Binary to Decimal Converter</h1>
+      <br />
+      <div>
+        {errorMessage && <span style={{ color: "red" }}>{errorMessage}</span>}
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="binary">Binary Input</label>
+          <br />
+          <input
+            type="text"
+            name="binary"
+            value={binary}
+            onChange={e => setBinary(e.target.value)}
+            placeholder="Enter 0 or 1"
+          />
+          <br />
+          <label htmlFor="decimal">Decimal Output</label>
+          <br />
+          <input type="text" value={decimalText} name="decimal" disabled />
+          <br />
+          <button>Convert</button>
+        </form>
+      </div>
     </div>
   );
 }
